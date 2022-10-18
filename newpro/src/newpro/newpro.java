0ccc393f739e0e79f8d4759e8b83837917573328
radhika2601 <115137695+radhika2601@users.noreply.github.com>
2022-10-18 15:19:00 +0530

    Update newpro.java

--- newpro/src/newpro/newpro.java
+++ newpro/src/newpro/newpro.java
@@ -1,9 +1,23 @@
 package newpro;
-
+import java.util.Scanner;
 public class newpro {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("Please enter Any three number:");
+		Scanner obj=new Scanner(System.in);
+		int n1=obj.nextInt();
+		int n2=obj.nextInt();
+		int n3=obj.nextInt();
+		int sum=n1+n2+n3;
+		if(sum==180)
+		{
+			System.out.println("Prize 1");
+		}
+		else
+		{
+			System.out.println(" No Prize");
+		}
 
 	}
 
